f2c0a840f1ca72da15665800b82acfe794a3a571
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-01 18:29:47 +0200

    Refactor DataCiteMetadataTest#getMockMetadata

--- src/test/java/org/gbif/doi/metadata/datacite/DataCiteMetadataTest.java
+++ src/test/java/org/gbif/doi/metadata/datacite/DataCiteMetadataTest.java
@@ -1,18 +1,31 @@
 package org.gbif.doi.metadata.datacite;
 
 import org.gbif.api.model.common.DOI;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.AlternateIdentifiers;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.AlternateIdentifiers.AlternateIdentifier;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Contributors;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Contributors.Contributor;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Contributors.Contributor.ContributorName;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Creators;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Creators.Creator;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Creators.Creator.CreatorName;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Dates;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Dates.Date;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Descriptions;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Descriptions.Description;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Formats;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.GeoLocations;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.GeoLocations.GeoLocation;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Identifier;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Publisher;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.RelatedIdentifiers;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.RelatedIdentifiers.RelatedIdentifier;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.RightsList;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.RightsList.Rights;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Sizes;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Subjects;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Subjects.Subject;
+import org.gbif.doi.metadata.datacite.DataCiteMetadata.Titles;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata.Titles.Title;
 import org.junit.Test;
 
@@ -29,71 +42,72 @@ public class DataCiteMetadataTest {
   public static DataCiteMetadata getMockMetadata(DOI doi, String title) {
     return DataCiteMetadata.builder()
         // build titles
-        .withTitles()
-        .addTitle(Title.builder()
-            .withLang("en-us")
-            .withValue(title)
+        .withTitles(Titles.builder()
+            .addTitle(Title.builder()
+                .withLang("en-us")
+                .withValue(title)
+                .build())
+            .addTitle(Title.builder()
+                .withLang("en-us")
+                .withTitleType(TitleType.SUBTITLE)
+                .withValue("Demonstration of DataCite Properties.")
+                .build())
             .build())
-        .addTitle(Title.builder()
-            .withLang("en-us")
-            .withTitleType(TitleType.SUBTITLE)
-            .withValue("Demonstration of DataCite Properties.")
-            .build())
-        .end()
 
         // build publisher
-        .withPublisher()
-        .withValue("DataCite")
-        .end()
+        .withPublisher(Publisher.builder()
+            .withValue("DataCite")
+            .build())
 
         // build publication year
         .withPublicationYear("2014")
 
         // build resource type
-        .withResourceType()
-        .withResourceTypeGeneral(ResourceType.SOFTWARE).withValue("XML")
-        .end()
+        .withResourceType(DataCiteMetadata.ResourceType.builder()
+            .withResourceTypeGeneral(ResourceType.SOFTWARE)
+            .withValue("XML")
+            .build())
 
         // build related identifiers
-        .withRelatedIdentifiers()
-        .addRelatedIdentifier(RelatedIdentifier.builder()
-            .withRelatedIdentifierType(RelatedIdentifierType.URL)
-            .withRelationType(RelationType.HAS_METADATA)
-            .withRelatedMetadataScheme("citeproc+json")
-            .withSchemeURI("https://github.com/citation-style-language/schema/raw/master/csl-data.json")
-            .withValue("http://data.datacite.org/application/citeproc+json/10.21373/example-full")
+        .withRelatedIdentifiers(RelatedIdentifiers.builder()
+            .addRelatedIdentifier(RelatedIdentifier.builder()
+                .withRelatedIdentifierType(RelatedIdentifierType.URL)
+                .withRelationType(RelationType.HAS_METADATA)
+                .withRelatedMetadataScheme("citeproc+json")
+                .withSchemeURI("https://github.com/citation-style-language/schema/raw/master/csl-data.json")
+                .withValue("http://data.datacite.org/application/citeproc+json/10.21373/example-full")
+                .build())
+            .addRelatedIdentifier(RelatedIdentifier.builder()
+                .withRelatedIdentifierType(RelatedIdentifierType.AR_XIV)
+                .withRelationType(RelationType.IS_REVIEWED_BY)
+                .withValue("arXiv:0706.0001")
+                .build())
             .build())
-        .addRelatedIdentifier(RelatedIdentifier.builder()
-            .withRelatedIdentifierType(RelatedIdentifierType.AR_XIV)
-            .withRelationType(RelationType.IS_REVIEWED_BY)
-            .withValue("arXiv:0706.0001")
-            .build())
-        .end()
 
         // build dates
-        .withDates()
-        .addDate(Date.builder()
-            .withDateType(DateType.UPDATED)
-            .withValue("2014-10-17")
+        .withDates(Dates.builder()
+            .addDate(Date.builder()
+                .withDateType(DateType.UPDATED)
+                .withValue("2014-10-17")
+                .build())
             .build())
-        .end()
 
         // build creators
-        .withCreators()
-        .addCreator(Creator.builder()
-            .withCreatorName(CreatorName.builder()
-                .withValue("Miller, Elizabeth")
-                .build())
-            .addNameIdentifier(NameIdentifier.builder()
-                .withSchemeURI("http://orcid.org")
-                .withNameIdentifierScheme("ORCID")
-                .withValue("http://orcid.org/0000-0001-5000-0007")
-                .build())
-            .addAffiliation(Affiliation.builder()
-                .withValue("DataCite")
+        .withCreators(Creators.builder()
+            .addCreator(Creator.builder()
+                .withCreatorName(CreatorName.builder()
+                    .withValue("Miller, Elizabeth")
+                    .build())
+                .addNameIdentifier(NameIdentifier.builder()
+                    .withSchemeURI("http://orcid.org")
+                    .withNameIdentifierScheme("ORCID")
+                    .withValue("http://orcid.org/0000-0001-5000-0007")
+                    .build())
+                .addAffiliation(Affiliation.builder()
+                    .withValue("DataCite")
+                    .build())
                 .build())
             .build())
-        .end()
 
         // build identifier
         .withIdentifier(Identifier.builder()
@@ -102,90 +116,90 @@ public class DataCiteMetadataTest {
             .build())
 
         // build alternate identifiers
-        .withAlternateIdentifiers()
-        .addAlternateIdentifier(AlternateIdentifier.builder()
-            .withAlternateIdentifierType("URL")
-            .withValue("http://schema.datacite.org/schema/meta/kernel-4/example/datacite-example-full-v4.xml")
+        .withAlternateIdentifiers(AlternateIdentifiers.builder()
+            .addAlternateIdentifier(AlternateIdentifier.builder()
+                .withAlternateIdentifierType("URL")
+                .withValue("http://schema.datacite.org/schema/meta/kernel-4/example/datacite-example-full-v4.xml")
+                .build())
             .build())
-        .end()
 
         // build descriptions
-        .withDescriptions()
-        .addDescription(Description.builder()
-            .withLang("en-us")
-            .withDescriptionType(DescriptionType.ABSTRACT)
-            .withContent("XML example of all DataCite Metadata Schema v3.1 properties.")
+        .withDescriptions(Descriptions.builder()
+            .addDescription(Description.builder()
+                .withLang("en-us")
+                .withDescriptionType(DescriptionType.ABSTRACT)
+                .withContent("XML example of all DataCite Metadata Schema v3.1 properties.")
+                .build())
             .build())
-        .end()
 
         // build language
         .withLanguage("en-us")
 
         // build rights list
-        .withRightsList()
-        .addRights(Rights.builder()
-            .withRightsURI("http://creativecommons.org/publicdomain/zero/1.0/")
-            .withValue("CC0 1.0 Universal")
+        .withRightsList(RightsList.builder()
+            .addRights(Rights.builder()
+                .withRightsURI("http://creativecommons.org/publicdomain/zero/1.0/")
+                .withValue("CC0 1.0 Universal")
+                .build())
             .build())
-        .end()
 
         // build geo locations
-        .withGeoLocations()
-        .addGeoLocation(GeoLocation.builder()
-            .addGeoLocationPoint(Point.builder()
-                .withPointLongitude(31.233f)
-                .withPointLatitude(-67.302f)
+        .withGeoLocations(GeoLocations.builder()
+            .addGeoLocation(GeoLocation.builder()
+                .addGeoLocationPoint(Point.builder()
+                    .withPointLongitude(31.233f)
+                    .withPointLatitude(-67.302f)
+                    .build())
+                .addGeoLocationBox(Box.builder()
+                    .withWestBoundLongitude(-71.032f)
+                    .withEastBoundLongitude(-68.211f)
+                    .withSouthBoundLatitude(41.090f)
+                    .withNorthBoundLatitude(42.893f)
+                    .build())
+                .addGeoLocationPlace("Atlantic Ocean")
                 .build())
-            .addGeoLocationBox(Box.builder()
-                .withWestBoundLongitude(-71.032f)
-                .withEastBoundLongitude(-68.211f)
-                .withSouthBoundLatitude(41.090f)
-                .withNorthBoundLatitude(42.893f)
-                .build())
-            .addGeoLocationPlace("Atlantic Ocean")
             .build())
-        .end()
 
         // build version
         .withVersion("3.1")
 
         // build sizes
-        .withSizes()
-        .addSize("3KB")
-        .end()
+        .withSizes(Sizes.builder()
+            .addSize("3KB")
+            .build())
 
         // build formats
-        .withFormats()
-        .addFormat("application/xml")
-        .end()
+        .withFormats(Formats.builder()
+            .addFormat("application/xml")
+            .build())
 
         // build contributors
-        .withContributors()
-        .addContributor(Contributor.builder()
-            .withContributorType(ContributorType.PROJECT_LEADER)
-            .withContributorName(ContributorName.builder()
-                .withValue("Starr, Joan")
-                .build())
-            .withNameIdentifier(NameIdentifier.builder()
-                .withSchemeURI("http://orcid.org/")
-                .withNameIdentifierScheme("ORCID")
-                .withValue("http://orcid.org/0000-0002-7285-027X")
-                .build())
-            .withAffiliation(Affiliation.builder()
-                .withValue("California Digital Library")
+        .withContributors(Contributors.builder()
+            .addContributor(Contributor.builder()
+                .withContributorType(ContributorType.PROJECT_LEADER)
+                .withContributorName(ContributorName.builder()
+                    .withValue("Starr, Joan")
+                    .build())
+                .withNameIdentifier(NameIdentifier.builder()
+                    .withSchemeURI("http://orcid.org/")
+                    .withNameIdentifierScheme("ORCID")
+                    .withValue("http://orcid.org/0000-0002-7285-027X")
+                    .build())
+                .withAffiliation(Affiliation.builder()
+                    .withValue("California Digital Library")
+                    .build())
                 .build())
             .build())
-        .end()
 
         // build subjects
-        .withSubjects()
-        .addSubject(Subject.builder()
-            .withLang("en-us")
-            .withSchemeURI("http://dewey.info/")
-            .withSubjectScheme("dewey")
-            .withValue("000 computer science")
+        .withSubjects(Subjects.builder()
+            .addSubject(Subject.builder()
+                .withLang("en-us")
+                .withSchemeURI("http://dewey.info/")
+                .withSubjectScheme("dewey")
+                .withValue("000 computer science")
+                .build())
             .build())
-        .end()
         .build();
   }
 
@@ -197,7 +211,7 @@ public class DataCiteMetadataTest {
     ObjectFactory of = new ObjectFactory();
     DataCiteMetadata res = of.createDataCiteMetadata();
 
-    DataCiteMetadata.Creators creators = of.createDataCiteMetadataCreators();
+    Creators creators = of.createDataCiteMetadataCreators();
     Creator creator = of.createDataCiteMetadataCreatorsCreator();
     CreatorName cn = of.createDataCiteMetadataCreatorsCreatorCreatorName();
     cn.setValue("Piyapong");
@@ -214,14 +228,14 @@ public class DataCiteMetadataTest {
     id.setValue("10.21373/WDCC/CCSRNIES_SRES_B2");
     res.setIdentifier(id);
 
-    DataCiteMetadata.Titles titles = of.createDataCiteMetadataTitles();
+    Titles titles = of.createDataCiteMetadataTitles();
     Title title = of.createDataCiteMetadataTitlesTitle();
     title.setValue("National Institute for Environmental Studies and Center for Climate System Research Japan");
     titles.getTitle().add(title);
     res.setTitles(titles);
 
     res.setPublicationYear("2014");
-    DataCiteMetadata.Publisher pub = of.createDataCiteMetadataPublisher();
+    Publisher pub = of.createDataCiteMetadataPublisher();
     pub.setValue("Piyapong");
     res.setPublisher(pub);
 
