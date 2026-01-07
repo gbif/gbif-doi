cec41fd6ef3b0a702e32af2807ce451d04087549
A.Hakan <37963260+ahakanzn@users.noreply.github.com>
2026-01-07 10:14:00 +0300

    java17 upgrade (#31)
    * upgrade to java17
    
    * fix version
    
    * fix format action

--- src/main/java/org/gbif/doi/metadata/datacite/ObjectFactory.java
+++ src/main/java/org/gbif/doi/metadata/datacite/ObjectFactory.java
@@ -13,9 +13,9 @@
  */
 package org.gbif.doi.metadata.datacite;
 
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.annotation.XmlElementDecl;
-import javax.xml.bind.annotation.XmlRegistry;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.annotation.XmlElementDecl;
+import jakarta.xml.bind.annotation.XmlRegistry;
 import javax.xml.namespace.QName;
 
 /**
